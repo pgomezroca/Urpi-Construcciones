cc1b905dd221fc05664453e7636f624c689fd989
Urpi <Urpi@MacBook-Air-de-Paula.local>
2025-03-15 13:21:49 -0300

    saque ingles, cree nosotros, y otros

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-//formulario
+// scripts.js
+
+// 1. Seleccionamos todos los enlaces de clase nav-link
+const navbarLinks = document.querySelectorAll('.nav-link');
+
+// 2. Iteramos sobre cada enlace para asignarle el comportamiento
+navbarLinks.forEach(link => {
+  link.addEventListener('mouseover', function handleMouseOver() {
+    // 3. Redirigimos inmediatamente al pasar el mouse
+    window.location.href = link.href;
+  });
+});
